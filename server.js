9b2718d1bfdf3d266a83ab09a895c6ee1de5f15a
Kalidasan <kalidasan760@gamil.com>
2026-09-04 06:56:38 +0530

    fix homepage route

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
 app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, 'index.html'));
+
+});
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
